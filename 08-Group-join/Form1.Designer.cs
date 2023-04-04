e5038357f46c0a9f00d2c00ac2849594704bd3ad
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-04-04 13:56:25 +0300

    linq group join examples added

---
+++ 08-Group-join/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _08_Group_join
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnYasaGoreGrupla = new System.Windows.Forms.Button();
+            this.btnPersonelDepartmanBilgileri = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(245, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(394, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnYasaGoreGrupla
+            // 
+            this.btnYasaGoreGrupla.Location = new System.Drawing.Point(12, 12);
+            this.btnYasaGoreGrupla.Name = "btnYasaGoreGrupla";
+            this.btnYasaGoreGrupla.Size = new System.Drawing.Size(178, 31);
+            this.btnYasaGoreGrupla.TabIndex = 1;
+            this.btnYasaGoreGrupla.Text = "Yaşa Göre Grupla";
+            this.btnYasaGoreGrupla.UseVisualStyleBackColor = true;
+            this.btnYasaGoreGrupla.Click += new System.EventHandler(this.btnYasaGoreGrupla_Click);
+            // 
+            // btnPersonelDepartmanBilgileri
+            // 
+            this.btnPersonelDepartmanBilgileri.Location = new System.Drawing.Point(12, 49);
+            this.btnPersonelDepartmanBilgileri.Name = "btnPersonelDepartmanBilgileri";
+            this.btnPersonelDepartmanBilgileri.Size = new System.Drawing.Size(178, 32);
+            this.btnPersonelDepartmanBilgileri.TabIndex = 1;
+            this.btnPersonelDepartmanBilgileri.Text = "Personel Departman Bilgileri";
+            this.btnPersonelDepartmanBilgileri.UseVisualStyleBackColor = true;
+            this.btnPersonelDepartmanBilgileri.Click += new System.EventHandler(this.btnPersonelDepartmanBilgileri_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.Controls.Add(this.btnPersonelDepartmanBilgileri);
+            this.Controls.Add(this.btnYasaGoreGrupla);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnYasaGoreGrupla;
+        private Button btnPersonelDepartmanBilgileri;
+    }
+}
---
+++ 08-Group-join/Form1.cs
@@ -0,0 +1,103 @@
+namespace _08_Group_join
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Departman> departmanlar = new List<Departman>()
+        {
+            new Departman(){DepartmanId=1,DepartmanAdi="Bilgi Ýþlem"},
+            new Departman(){DepartmanId=2,DepartmanAdi="Muhasebe"},
+            new Departman(){DepartmanId=3,DepartmanAdi="Satýn Alma"},
+        };
+
+
+
+
+
+        List<Personel> personeller = new List<Personel>()
+        {
+            new Personel(){PersonelId=1, Adi="Baris",Yas=38, DepartmanId=1},
+            new Personel(){PersonelId=2, Adi="Irem",Yas=25, DepartmanId=1},
+            new Personel(){PersonelId=3, Adi="Muhammed",Yas=25, DepartmanId=2},
+            new Personel(){PersonelId=4, Adi="Aykut",Yas=38, DepartmanId=3},
+            new Personel(){PersonelId=5, Adi="Umut",Yas=23, DepartmanId=3},
+        };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYasaGoreGrupla_Click(object sender, EventArgs e)
+        {
+            //AMAÇ satýr 1: 38- Ayþe, Aykut
+            //satýr 2 : 25- Fatma, Ali
+            var list = personeller.GroupBy(x=> x.Yas).ToList();
+
+            List<PersonelSatir> yeniPersonelListesi = new List<PersonelSatir>();
+            foreach (var item in list)
+            {
+                PersonelSatir personelSatir = new PersonelSatir();
+                personelSatir.Yas = item.Key;
+                personelSatir.Isimler = string.Join(" , ", item.Select(x => x.Adi));
+
+                yeniPersonelListesi.Add(personelSatir);
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = yeniPersonelListesi;
+
+        }
+
+        private void btnPersonelDepartmanBilgileri_Click(object sender, EventArgs e)
+        {
+            //Çýktý
+            //PersonelinAdi, DepartmaninAdi
+            var sonuc = personeller.Join(
+                                    departmanlar, 
+                                    p => p.DepartmanId, 
+                                    d => d.DepartmanId,
+                                    (personel,departman) => new PersonelDepartman
+                                    {
+                                        PersonelAdi = personel.Adi,
+                                        DepartmanAdi = departman.DepartmanAdi
+                                    }
+                                    ).ToList();
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = sonuc;
+
+
+        }
+    }
+}
+
+public class Personel
+{
+    public int PersonelId { get; set; }
+    public string Adi { get; set; }
+    public int Yas { get; set; }
+    public int DepartmanId { get; set; }
+}
+
+public class Departman
+{
+    public int DepartmanId { get; set; }
+    public string DepartmanAdi { get; set; }
+}
+
+public class PersonelSatir
+{
+    public int Yas { get; set; }
+    public string Isimler { get; set; }
+}
+
+public class PersonelDepartman
+{
+    public string PersonelAdi { get; set; }
+    public string DepartmanAdi { get; set; }
+}
